db6eac8c8067110435a0a47c6936f87179252ec7
Finomnis <finomnis@gmail.com>
2021-12-16 23:56:05 +0100

    Add day16 part2

--- src/solutions/day16.rs
+++ src/solutions/day16.rs
@@ -35,17 +35,41 @@ pub struct Packet {
 #[derive(Debug)]
 pub enum Payload {
     Literal(u64),
-    Operator(Vec<Packet>),
+    Operator(Instruction, Vec<Packet>),
 }
 
+#[derive(Debug)]
 pub enum Instruction {
     Sum,
     Product,
     Minimum,
     Maximum,
     Greater,
-    Les,
+    Less,
     Equal,
+}
+
+impl Packet {
+    pub fn evaluate(&self) -> u64 {
+        self.payload.evaluate()
+    }
+}
+
+impl Payload {
+    pub fn evaluate(&self) -> u64 {
+        match self {
+            Payload::Literal(val) => *val,
+            Payload::Operator(instruction, children) => match instruction {
+                Instruction::Sum => children.iter().map(Packet::evaluate).sum(),
+                Instruction::Product => children.iter().map(Packet::evaluate).product(),
+                Instruction::Minimum => children.iter().map(Packet::evaluate).min().unwrap(),
+                Instruction::Maximum => children.iter().map(Packet::evaluate).max().unwrap(),
+                Instruction::Greater => (children[0].evaluate() > children[1].evaluate()) as u64,
+                Instruction::Less => (children[0].evaluate() < children[1].evaluate()) as u64,
+                Instruction::Equal => (children[0].evaluate() == children[1].evaluate()) as u64,
+            },
+        }
+    }
 }
 
 impl std::fmt::Display for Packet {
@@ -57,8 +81,25 @@ impl std::fmt::Display for Payload {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Payload::Literal(num) => write!(f, "{}", num),
-            Payload::Operator(_) => write!(f, "op"),
+            Payload::Operator(instruction, _) => write!(f, "{}", instruction),
         }
+    }
+}
+impl std::fmt::Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Instruction::Sum => "sum",
+                Instruction::Product => "product",
+                Instruction::Minimum => "min",
+                Instruction::Maximum => "max",
+                Instruction::Greater => "greater",
+                Instruction::Less => "less",
+                Instruction::Equal => "equal",
+            }
+        )
     }
 }
 
@@ -84,14 +125,13 @@ mod parse {
         Some(Payload::Literal(val))
     }
 
-    fn operand(stream: &mut impl Iterator<Item = bool>) -> Option<Payload> {
+    fn operator(stream: &mut impl Iterator<Item = bool>, payload_type: u8) -> Option<Payload> {
         let length_as_count = stream.next()?;
-        if length_as_count {
+        let subpackets = if length_as_count {
             let count: Int<11> = parse::int(stream)?;
-            let subpackets = (0..count.to_num::<usize>())
+            (0..count.to_num::<usize>())
                 .map(|_| parse::packet(stream))
-                .collect::<Option<Vec<_>>>()?;
-            Some(Payload::Operator(subpackets))
+                .collect::<Option<Vec<_>>>()?
         } else {
             let length: Int<15> = parse::int(stream)?;
             let mut data = (0..length.to_num::<usize>())
@@ -103,15 +143,28 @@ mod parse {
             while data.peek().is_some() {
                 subpackets.push(parse::packet(&mut data)?);
             }
-            Some(Payload::Operator(subpackets))
-        }
+            subpackets
+        };
+
+        let instruction = match payload_type {
+            0 => Instruction::Sum,
+            1 => Instruction::Product,
+            2 => Instruction::Minimum,
+            3 => Instruction::Maximum,
+            5 => Instruction::Greater,
+            6 => Instruction::Less,
+            7 => Instruction::Equal,
+            _ => panic!("Unknown payload type: {}", payload_type),
+        };
+
+        Some(Payload::Operator(instruction, subpackets))
     }
 
     fn payload(stream: &mut impl Iterator<Item = bool>) -> Option<Payload> {
         let payload_type: Int<3> = parse::int(stream)?;
         match payload_type {
             Int([true, false, false]) => parse::literal(stream),
-            _ => parse::operand(stream),
+            _ => parse::operator(stream, payload_type.to_num()),
         }
     }
 
@@ -138,7 +191,7 @@ fn hex_to_binary_stream(input_data: &str) -> impl Iterator<Item = bool> + '_ {
 fn packet_tree_to_string(packet: &Packet) -> Result<String, Box<dyn std::error::Error>> {
     let mut result = String::new();
     writeln!(result, "{}", packet)?;
-    if let Payload::Operator(children) = &packet.payload {
+    if let Payload::Operator(_, children) = &packet.payload {
         for child in children
             .iter()
             .map(|child| packet_tree_to_string(child))
@@ -156,7 +209,7 @@ fn get_accumulated_version_numbers(packet: &Packet) -> usize {
     packet.version.to_num::<usize>()
         + match &packet.payload {
             Payload::Literal(_) => 0,
-            Payload::Operator(children) => {
+            Payload::Operator(_, children) => {
                 children.iter().map(get_accumulated_version_numbers).sum()
             }
         }
@@ -164,14 +217,18 @@ fn get_accumulated_version_numbers(packet: &Packet) -> usize {
 
 pub fn task1(input_data: &str) -> usize {
     let stream = &mut hex_to_binary_stream(input_data);
-
     let packet = parse::packet(stream).unwrap();
 
     get_accumulated_version_numbers(&packet)
 }
 
-pub fn task2(_input_data: &str) -> u64 {
-    0
+pub fn task2(input_data: &str) -> u64 {
+    let stream = &mut hex_to_binary_stream(input_data);
+    let packet = parse::packet(stream).unwrap();
+
+    //println!("{}", packet_tree_to_string(&packet).unwrap());
+
+    packet.evaluate()
 }
 
 crate::aoc_tests! {
@@ -184,6 +241,14 @@ crate::aoc_tests! {
         complex => 897,
     },
     task2: {
+        sum => 3,
+        product => 54,
+        min => 7,
+        max => 9,
+        less => 1,
+        greater => 0,
+        equal => 0,
+        simple5 => 1,
         complex => 0,
     }
 }
