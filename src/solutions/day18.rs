169a1d48061795f7f3df5d1eb7973b78b6cc3bde
Finomnis <finomnis@gmail.com>
2021-12-19 12:55:40 +0100

    More work on day18

--- src/solutions/day18.rs
+++ src/solutions/day18.rs
@@ -10,39 +10,103 @@ mod parser {
     };
 
     pub fn regular(input: &str) -> IResult<&str, SnailfishNumber> {
-        map(u64, |num| SnailfishNumber::Number(num))(input)
+        map(u64, SnailfishNumber::regular)(input)
     }
 
-    pub fn pair(input: &str) -> IResult<&str, SnailfishNumber> {
-        map(
-            delimited(
-                tag("["),
-                separated_pair(snailfish_number, tag(","), snailfish_number),
-                tag("]"),
-            ),
-            |(left, right)| SnailfishNumber::Pair(Box::new(left), Box::new(right)),
-        )(input)
+    pub fn member(input: &str) -> IResult<&str, SnailfishNumber> {
+        alt((regular, snailfish_number))(input)
     }
 
     pub fn snailfish_number(input: &str) -> IResult<&str, SnailfishNumber> {
-        alt((regular, pair))(input)
+        map(
+            delimited(tag("["), separated_pair(member, tag(","), member), tag("]")),
+            SnailfishNumber::pair,
+        )(input)
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum SnailfishNumber {
-    Number(u64),
-    Pair(Box<Self>, Box<Self>),
+    Regular(u64),
+    Pair(Box<SnailfishNumber>, Box<SnailfishNumber>),
+}
+
+impl SnailfishNumber {
+    pub fn regular(num: u64) -> Self {
+        Self::Regular(num)
+    }
+    pub fn pair((left, right): (SnailfishNumber, SnailfishNumber)) -> Self {
+        Self::Pair(Box::new(left), Box::new(right))
+    }
 }
 
 impl std::fmt::Display for SnailfishNumber {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            SnailfishNumber::Number(num) => write!(f, "{}", num),
+            SnailfishNumber::Regular(num) => write!(f, "{}", num),
             SnailfishNumber::Pair(left, right) => write!(f, "[{},{}]", left, right),
         }
     }
 }
+
+// pub enum ReduceResult {
+//     Ok,
+//     NeedsExplode(u64, SnailfishNumberElement, u64)
+// }
+
+// impl SnailfishNumber {
+//     pub fn reduce(mut self) -> Self {
+//         match self.reduce_impl(0) {
+//             ReduceResult::Ok => (),
+//             ReduceResult::NeedsExplode(_, _, _) => todo!(),
+//         }
+//         self
+//     }
+
+//     pub fn reduce_impl(&mut self, depth: usize) -> ReduceResult {
+//         if let SnailfishNumber(
+//             SnailfishNumberElement::Regular(left),
+//             SnailfishNumberElement::Regular(right),
+//         ) = &self
+//         {
+//             if depth > 4 {
+//                 println!("Explode {}, {}", left, right);
+//             }
+//         }
+
+//         self.0.reduce(depth)
+
+//         ReduceResult::NoActionNeeded(self)
+//     }
+// }
+
+// impl SnailfishNumberElement {
+//     pub fn reduce(self, depth: usize) -> (Self, ReduceResult) {}
+// }
+
+// impl std::ops::Add for SnailfishNumber {
+//     type Output = Self;
+
+//     fn add(self, rhs: Self) -> Self::Output {
+//         SnailfishNumber(
+//             SnailfishNumberElement::snailfish(self),
+//             SnailfishNumberElement::snailfish(rhs),
+//         )
+//         .reduce()
+//     }
+// }
+
+// impl<'a, 'b> std::ops::Add<&'b SnailfishNumber> for &'a SnailfishNumber {
+//     type Output = SnailfishNumber;
+
+//     fn add(self, rhs: &'b SnailfishNumber) -> Self::Output {
+//         SnailfishNumber(
+//             SnailfishNumberElement::snailfish(self.clone()),
+//             SnailfishNumberElement::snailfish(rhs.clone()),
+//         )
+//         .reduce()
+//     }
+// }
 
 pub fn parse_input(input_data: &str) -> Vec<SnailfishNumber> {
     input_data
@@ -53,9 +117,18 @@ pub fn parse_input(input_data: &str) -> Vec<SnailfishNumber> {
 }
 
 pub fn task1(numbers: &[SnailfishNumber]) -> u64 {
+    let numbers = numbers.to_vec();
+
     for number in numbers {
         println!("{}", number);
     }
+    // numbers.into_iter().reduce(|prev, acc| {
+    //     print!("{} + {} = ", prev, acc);
+    //     let result = prev + acc;
+    //     println!("{}", result);
+    //     result
+    // });
+
     0
 }
 
