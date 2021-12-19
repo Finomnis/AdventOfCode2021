7586d41da77637d375471fe4c58db6cc51f4a20e
Finomnis <finomnis@gmail.com>
2021-12-19 18:11:56 +0100

    More ideas on day19

--- src/solutions/day19.rs
+++ src/solutions/day19.rs
@@ -42,6 +42,11 @@ mod parser {
     }
 }
 
+pub fn parse_input(input_data: &str) -> Vec<Scanner> {
+    let (_, scanners) = parser::scanners(input_data.trim()).unwrap();
+    scanners
+}
+
 fn get_distance_hash(p0: &Pos, p1: &Pos) -> u32 {
     (p1.0 - p0.0).abs() as u32 + (p1.1 - p0.1).abs() as u32 + (p1.2 - p0.2).abs() as u32
 }
@@ -53,46 +58,79 @@ pub struct Pos(i32, i32, i32);
 pub struct Scanner {
     number: u64,
     beacons: Vec<Pos>,
-    distances_per_beacon: Vec<HashSet<u32>>,
-    distances: HashSet<u32>,
+    coords_per_axis: [Vec<i32>; 3],
+    coord_delta_histo_per_axis: [HashSet<u32>; 3],
 }
 impl Scanner {
     pub fn new((number, beacons): (u64, Vec<Pos>)) -> Self {
-        let distances_per_beacon = beacons
-            .iter()
-            .map(|beacon| {
-                beacons
-                    .iter()
-                    .map(|other| get_distance_hash(beacon, other))
-                    .filter(|dist| *dist != 0)
-                    .collect::<HashSet<_>>()
-            })
-            .collect::<Vec<_>>();
-        let distances = distances_per_beacon
-            .iter()
-            .chain_nested_iterator(|distances| distances.iter())
-            .cloned()
-            .collect::<HashSet<_>>();
+        let mut coords_per_axis =
+            beacons
+                .iter()
+                .fold([Vec::new(), Vec::new(), Vec::new()], |mut dists, beacon| {
+                    dists[0].push(beacon.0);
+                    dists[1].push(beacon.1);
+                    dists[2].push(beacon.2);
+                    dists
+                });
+
+        coords_per_axis[0].sort();
+        coords_per_axis[1].sort();
+        coords_per_axis[2].sort();
+        coords_per_axis[0].dedup();
+        coords_per_axis[1].dedup();
+        coords_per_axis[2].dedup();
+
+        let coord_delta_histo_per_axis = [
+            coords_per_axis[0]
+                .windows(2)
+                .map(|elems| (elems[1] - elems[0]).abs() as u32)
+                .collect::<HashSet<_>>(),
+            coords_per_axis[1]
+                .windows(2)
+                .map(|elems| (elems[1] - elems[0]).abs() as u32)
+                .collect::<HashSet<_>>(),
+            coords_per_axis[2]
+                .windows(2)
+                .map(|elems| (elems[1] - elems[0]).abs() as u32)
+                .collect::<HashSet<_>>(),
+        ];
         Self {
             number,
             beacons,
-            distances,
-            distances_per_beacon,
+            coords_per_axis,
+            coord_delta_histo_per_axis,
         }
     }
 
-    pub fn overlap_score(&self, other: &Scanner) -> usize {
-        self.distances.union(&other.distances).count()
+    pub fn overlap_score(&self, other: &Scanner) -> (usize, (usize, usize, usize)) {
+        [
+            (0, 1, 2),
+            (0, 2, 1),
+            (1, 0, 2),
+            (1, 2, 0),
+            (2, 0, 1),
+            (2, 1, 0),
+        ]
+        .into_iter()
+        .map(|order| {
+            let score_0 = self.coord_delta_histo_per_axis[0]
+                .union(&other.coord_delta_histo_per_axis[order.0])
+                .count();
+            let score_1 = self.coord_delta_histo_per_axis[1]
+                .union(&other.coord_delta_histo_per_axis[order.1])
+                .count();
+            let score_2 = self.coord_delta_histo_per_axis[2]
+                .union(&other.coord_delta_histo_per_axis[order.2])
+                .count();
+            (score_0 + score_1 + score_2, order)
+        })
+        .max_by_key(|(key, _)| *key)
+        .unwrap_or((0, (0, 0, 0)))
     }
 }
 
-pub fn parse_input(input_data: &str) -> Vec<Scanner> {
-    let (_, scanners) = parser::scanners(input_data.trim()).unwrap();
-    scanners
-}
-
 pub fn task1(scanners: &[Scanner]) -> u64 {
-    for (score, scanner1, scanner2) in scanners
+    for ((score, alignment), scanner1, scanner2) in scanners
         .iter()
         .tuple_combinations()
         .map(|(left, right)| {
@@ -102,8 +140,8 @@ pub fn task1(scanners: &[Scanner]) -> u64 {
         .sorted_by_key(|t| Reverse(t.0))
     {
         println!(
-            "Score: {}, ({}, {})",
-            score, scanner1.number, scanner2.number
+            "Score: {}, ({}, {}) - align: ({:?})",
+            score, scanner1.number, scanner2.number, alignment
         );
     }
 
